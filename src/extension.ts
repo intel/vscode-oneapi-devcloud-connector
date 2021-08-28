528f07c845bc5451afe5c0f556561ebf4260b378
Sergey Bobko <sergey.bobko@intel.com>
2021-08-28 16:43:57 +0300

    Renamed devcloud to intel-corporation.oneapi-devcloud-connect

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,9 @@ import { DevConnect } from './devconnect';
 
 export function activate(context: vscode.ExtensionContext): void {
 	const devcloud = new DevConnect();
-	context.subscriptions.push(vscode.commands.registerCommand('devcloud.help', () => devcloud.getHelp()));
-	context.subscriptions.push(vscode.commands.registerCommand('devcloud.setupConnection', () => devcloud.setupConnection()));
-	context.subscriptions.push(vscode.commands.registerCommand('devcloud.closeConnection', ()  => devcloud.closeConnection()));
+	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-devcloud-connect.help', () => devcloud.getHelp()));
+	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-devcloud-connect.setupConnection', () => devcloud.setupConnection()));
+	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-devcloud-connect.closeConnection', ()  => devcloud.closeConnection()));
 }
 
 export function deactivate(): void { return; }
