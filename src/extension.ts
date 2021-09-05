3f154d7241ecec9e50c0e499acde1edb61099fb3
Vladimir Sorokin <vladimir.sorokin@intel.com>
2021-09-05 19:26:31 +0300

    Add "devcloud working terminal " command

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,8 @@ import { DevConnect } from './devconnect';
 
 export function activate(context: vscode.ExtensionContext): void {
 	const devcloud = new DevConnect();
-
+	
+	devcloud.isConnected = false;
 	devcloud.proxy = vscode.workspace.getConfiguration("intel-corporation.oneapi-devcloud-connect").get<boolean>("proxy");
 	devcloud.connectionTimeout = vscode.workspace.getConfiguration("intel-corporation.oneapi-devcloud-connect").get<number>('connection_timeout');
 	devcloud.jobTimeout = vscode.workspace.getConfiguration("intel-corporation.oneapi-devcloud-connect").get<string>('job_timeout');
@@ -30,6 +31,8 @@ export function activate(context: vscode.ExtensionContext): void {
 	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-devcloud-connect.help', () => devcloud.getHelp()));
 	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-devcloud-connect.setupConnection', () => devcloud.setupConnection()));
 	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-devcloud-connect.closeConnection', () => devcloud.closeConnection()));
+	context.subscriptions.push(vscode.commands.registerCommand('intel-corporation.oneapi-devcloud-connect.devcloudTerminal', () => devcloud.createDevCloudTerminal()));
+	
 }
 
 export function deactivate(): void { return; }
